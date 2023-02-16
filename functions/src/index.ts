95639bebf31b31abd0789956a994c1d64456d1ed
Igor Pereira <igorpereira@MacBook-Pro.bbrouter>
2023-02-16 15:09:30 -0300

    Fixed routhing on index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,6 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
-import * as serviceAccount from "./serviceAccountKey.json";
+import * as serviceAccount from "../lib/serviceAccountKey.json";
 
 admin.initializeApp({
   credential: admin.credential.cert({
